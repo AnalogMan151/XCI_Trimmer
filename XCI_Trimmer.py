b3609f21afe8835d267868fa80bfdb265e767f5b
AnalogMan151 <james.p.wolfe@outlook.com>
2018-05-22 13:55:15 -0400

    Added additional checks and copy file option

--- XCI_Trimmer.py
+++ XCI_Trimmer.py
@@ -6,21 +6,30 @@
 
 import os
 import argparse
+from shutil import copy2
 
+# Global variables
 filename = ""
 ROM_size = 0
 padding_offset = 0
+filesize = 0
+cartsize = 0
+copy_bool = False
 
+# Retrieve N bytes of data in big endian from passed data set starting at passed offset
 def readBE(data, offset, n):
     return (int.from_bytes(data[offset:offset+(n)], byteorder='big'))
 
+# Retrieve N bytes of data in little endian from passed data set starting at passed offset
 def readLE(data, offset, n):
     return (int.from_bytes(data[offset:offset+(n)], byteorder='little'))
 
+# Obtain expected ROM size and game data size (in GiB) from XCI header along with address padding begins
 def getSizes():
     ROM_size = 0
     Data_size = 0.0
     padding_offset = 0
+
     with open(filename, 'rb') as f:
         XCI = f.read(512)
         cart_size = readBE(XCI, 0x10C, 2)
@@ -41,9 +50,14 @@ def getSizes():
 
     return ROM_size, Data_size, padding_offset
 
-
+# Check if file is already trimmed. If not, verify padding has no unexpected data. If not, truncate file at padding address
 def trim():
+    if filesize == padding_offset:
+        print('ROM is already trimmed')
+        return
+
     print('Checking for data in padding...')
+
     with open(filename, 'rb') as f:
         f.seek(padding_offset)
         while True:
@@ -52,24 +66,49 @@ def trim():
                 # eof
                 break
             if b != b'\xFF':
-                print('Unexpected data found in padding! Aborting Trim.')
+                print('Unexpected data found in padding at 0x{:X}! Aborting Trim.'.format(f.tell()))
                 return
     quicktrim()
 
+# Check if copy file flag is set and copy file if so. Truncate at padding address
 def quicktrim():
-        print('Trimming {:s}...\n'.format(filename))
-        with open(filename, 'r+b') as f:
-            f.seek(padding_offset)
-            f.truncate()
+    global filename, copy_bool
 
+    if filesize == padding_offset:
+        print('ROM is already trimmed')
+        return
+
+    print('Trimming {:s}...\n'.format(filename))
+
+    if copy_bool:
+        copypath = filename[:-4] + '_trimmed.xci'
+        copy2(filename, copypath)
+        filename = copypath
+
+    with open(filename, 'r+b') as f:
+        f.seek(padding_offset)
+        f.truncate()
+
+# Check if file is already padded. If not, check if copy file flag is set and copy file if so. Add padding to end of file until file reached cart size
 def pad():
+    global filename, copy_bool
+
     padding = bytearray()
+
     print('Padding {:s}...\n'.format(filename))
-    i = ((ROM_size * 1024 - (ROM_size * 0x48)) * 1024 * 1024) - padding_offset
+
+    if filesize == cartsize:
+        print('ROM is already padded')
+        return
+
+    if copy_bool:
+        copypath = filename[:-4] + '_padded.xci'
+        copy2(filename, copypath)
+        filename = copypath
+
+    i = cartsize - filesize
+
     with open(filename, 'ab') as f:
-        if f.tell() > padding_offset:
-            print('ROM is already padded')
-            return
         padding += b'\xFF' * i
         f.write(padding)
             
@@ -83,6 +122,7 @@ def main():
     group.add_argument('-t', '--trim', action='store_true', help='Trim excess bytes')
     group.add_argument('-qt', '--quicktrim', action='store_true', help='Trims without safety check for unexpected game data')
     group.add_argument('-p', '--pad', action='store_true', help='Restore excess bytes')
+    parser.add_argument('-c', '--copy', action='store_true', help='Creates a copy instead of modifying original file')
 
     # Check passed arguments
     args = parser.parse_args()
@@ -92,17 +132,29 @@ def main():
         print('ROM cannot be found\n')
         return 1
     
-    global filename, ROM_size, padding_offset
+    global filename, ROM_size, padding_offset, filesize, cartsize, copy_bool
     filename = args.filename
 
     ROM_size, Data_size, padding_offset = getSizes()
+
+    # If ROM_size does not match one of the expected values, abort
     if ROM_size == 0:
         print('Could not determine ROM size. Sizes supported: 2G, 4G, 8G, 16G, 32G\n')
         return 1
 
+    filesize = os.path.getsize(filename)
+    cartsize = (ROM_size * 1024 - (ROM_size * 0x48)) * 1024 * 1024
+
+    # If filesize is too small or too large, abort
+    if filesize < padding_offset or filesize > cartsize:
+        print('ROM is improperly trimmed or padded. Aborting.\n')
+        return 1
+
     print('ROM  Size:     {:5d} GiB'.format(ROM_size))
     print('Trim Size:     {:5.2f} GiB\n'.format(Data_size))
-    
+
+    if args.copy:
+        copy_bool = True
     if args.trim:
         trim()
     if args.quicktrim:
@@ -110,7 +162,7 @@ def main():
     if args.pad:
         pad()
 
-    print('Done!\n\n')
+    print('Done!\n')
 
 if __name__ == "__main__":
     main()
